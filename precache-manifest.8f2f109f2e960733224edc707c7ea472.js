6f9fd104f5fcf53c65e7677464909b519efaa50a
snoopysnake <chiang4@gmail.com>
2020-10-15 03:09:36 -0400

    Updates

---
+++ precache-manifest.8f2f109f2e960733224edc707c7ea472.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e1a393cf1375bc17c40a2b45be1b3c",
+    "url": "/darpan/index.html"
+  },
+  {
+    "revision": "ffa6c67b9f3773d9087d",
+    "url": "/darpan/static/css/main.cff61cec.chunk.css"
+  },
+  {
+    "revision": "e354d40c00d09fb4aafe",
+    "url": "/darpan/static/js/2.004d0ab5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/darpan/static/js/2.004d0ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffa6c67b9f3773d9087d",
+    "url": "/darpan/static/js/main.a16d63fb.chunk.js"
+  },
+  {
+    "revision": "9b6f29c1ac8f1c130b57",
+    "url": "/darpan/static/js/runtime-main.86b689cb.js"
+  },
+  {
+    "revision": "3f4fe8b06cd7a0ae9b933abc84fe2971",
+    "url": "/darpan/static/media/cloud1.3f4fe8b0.svg"
+  },
+  {
+    "revision": "f659b38f9cdef76078f68634f3db8a6a",
+    "url": "/darpan/static/media/cloud2.f659b38f.svg"
+  },
+  {
+    "revision": "feeca4b2c577fdb994e85997afc0fe02",
+    "url": "/darpan/static/media/cloud3.feeca4b2.svg"
+  },
+  {
+    "revision": "a90afb63290cccf22bc4f9b5ea2555ac",
+    "url": "/darpan/static/media/cloud4.a90afb63.svg"
+  },
+  {
+    "revision": "bb029ae049208623cbb546607fe6ec68",
+    "url": "/darpan/static/media/cloud5.bb029ae0.svg"
+  },
+  {
+    "revision": "34aabc2a32a54670de8a9dfe63169474",
+    "url": "/darpan/static/media/cloud6.34aabc2a.svg"
+  },
+  {
+    "revision": "5a2b1e67d6408868804f65753f0418fc",
+    "url": "/darpan/static/media/plane1.5a2b1e67.svg"
+  },
+  {
+    "revision": "007d1364b0c690d3bac54055a04938c7",
+    "url": "/darpan/static/media/sun.007d1364.svg"
+  }
+]);
